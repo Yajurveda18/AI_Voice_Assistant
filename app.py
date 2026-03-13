0d5490eb8f16630d3a03bc5da22e0c437493e651
Yajurveda18 <yajurveda18@gmail.com>
2026-03-13 22:05:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 import streamlit as st
 import wikipedia
+from gtts import gTTS
 
-st.title("AI Assistant")
+st.title("AI Voice Assistant")
 
 user_input = st.text_input("Ask something")
 
 if user_input:
+
     if "wikipedia" in user_input:
         topic = user_input.replace("wikipedia","")
         result = wikipedia.summary(topic, sentences=2)
-        st.write(result)
 
     elif "hello" in user_input:
-        st.write("Hello, how can I help you?")
+        result = "Hello, how can I help you?"
+
+    else:
+        result = "I could not understand"
+
+    st.write(result)
+
+    # Convert text to speech
+    tts = gTTS(result)
+    tts.save("voice.mp3")
+
+    # Play audio in browser
+    audio_file = open("voice.mp3", "rb")
+    st.audio(audio_file.read(), format="audio/mp3")
